eaaf19ba77f1433ad6852af33e56c28bca3473a9
me.luc <lukasalmeida372@gmail.com>
2023-02-10 20:42:30 -0300

    add game schema

---
+++ src/schema/game.schema.js
@@ -0,0 +1,8 @@
+import Joi from "joi";
+
+export const gameSchema = Joi.object({
+	name: Joi.string().min(2).required(),
+	image: Joi.string().uri().required(),
+	stockTotal: Joi.number().required(),
+	pricePerDay: Joi.number().required(),
+});
