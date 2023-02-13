9abb5a9397e60369acbc35ab38e1e98d488a0582
me.luc <lukasalmeida372@gmail.com>
2023-02-13 16:00:32 -0300

    fix: game stock total minimum

--- src/schema/game.schema.js
+++ src/schema/game.schema.js
@@ -3,6 +3,6 @@ import Joi from "joi";
 export const gameSchema = Joi.object({
 	name: Joi.string().min(2).required(),
 	image: Joi.string().uri().required(),
-	stockTotal: Joi.number().required(),
+	stockTotal: Joi.number().min(0).required(),
 	pricePerDay: Joi.number().required(),
 });
